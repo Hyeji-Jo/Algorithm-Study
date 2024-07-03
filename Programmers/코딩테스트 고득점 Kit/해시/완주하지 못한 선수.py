6cb45f4a75f8f1263112661c21f71eba5091ec43
Hyebit <61963922+Hyeji-Jo@users.noreply.github.com>
2024-07-03 12:26:23 +0900

    완주하지 못한 선수.py

---
+++ Programmers/코딩테스트 고득점 Kit/해시/완주하지 못한 선수.py
@@ -0,0 +1,8 @@
+def solution(participant, completion):
+    answer = ''
+    for i in participant:
+        if i in completion:
+            completion.remove(i)
+        else:
+            answer=i
+    return answer
